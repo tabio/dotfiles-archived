57b23afba30b8aae54342f251584254faf6ec6bc
tabio <tabio.github@gmail.com>
2015-05-22 09:06:10 +0900

    mod setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,4 @@
 #! /bin/bash
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.vim ~/.vim
-ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
-ln -sf ~/dotfiles/.gitignore_global ~/.gitignore_global
+ln -sf ~/dotfiles/.gitignore ~/.gitignore
